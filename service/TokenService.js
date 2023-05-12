0ba824ba7d1a21f2251f739a1507cbc513b00c3c
IhorDobryanskiy <l48507100120@gmail.com>
2023-05-12 14:02:01 +0300

    change jwt lifecycle

--- service/TokenService.js
+++ service/TokenService.js
@@ -4,7 +4,7 @@ const tokenModel = require('../models/tokenModel');
 class TokenService {
   generateTokens(payload) {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_KEY, {
-      expiresIn: '15s'
+      expiresIn: '2h'
     });
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_KEY, {
       expiresIn: '30d'
